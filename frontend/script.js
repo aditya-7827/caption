1e7b4ca4af7add48c4c6a8339353d528b72868b3
aditya-7827 <aditya07827@gmail.com>
2026-05-12 23:34:36 +0530

    updated code

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";// =====================
+const BASE_URL = "https://caption-backend-33ra.onrender.com/";// =====================
 // Sections
 // =====================
 const registerBox = document.getElementById("registerBox");
